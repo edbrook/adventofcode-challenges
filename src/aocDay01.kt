1cee324aa20ca82e2389531cf2a4c51f886df06f
Ed Brook <edbrook@gmail.com>
2017-10-07 23:56:11 +0100

    Completed day 1

--- src/aocDay01.kt
+++ src/aocDay01.kt
@@ -7,59 +7,78 @@ import java.nio.file.Paths
 fun main(args: Array<String>) {
     val path = Paths.get("").toAbsolutePath().toString()
 
-    val directions = File("$path/input.txt")
+    val moves = File("$path/input.txt")
             .inputStream()
             .bufferedReader()
             .use { it.readLine().split(", ") }
 
-    val tc = TaxiCab(DEBUG = true)
-    for (move in directions) {
-        tc.processMove(move)
-    }
-    val dist = tc.getDistanceFromStart()
-    val dir = TaxiCab.directionToName(tc.getFinalDirection())
+    val tc = TaxiCab()
+    var dist = 0
 
-    println("Distance from start: $dist")
-    println("Final direction: $dir")
+    for (move in moves) {
+        val crossed = tc.processMove(move)
+        if (crossed.size > 0) {
+            dist = tc.getDistanceFromStart(crossed[0])
+            println("Crossed at: ${crossed[0]}")
+            break
+        }
+    }
+
+    println("Distance to first crossed path from start: $dist")
 }
 
 
 class TaxiCab(private val DEBUG: Boolean = false) {
     companion object {
         val NORTH = 0
-        val EAST = 1
+        val EAST  = 1
         val SOUTH = 2
-        val WEST = 3
+        val WEST  = 3
 
         private data class Move(val rot: String, val by: Int)
+        data class MapLocation(val ew: Int, val ns: Int) {
+            override fun toString(): String {
+                return "($ew, $ns)"
+            }
+        }
 
         fun directionToName(dir: Int): String {
             return when (dir) {
                 NORTH -> "North"
-                EAST -> "East"
+                EAST  -> "East"
                 SOUTH -> "South"
-                WEST -> "West"
-                else -> throw IllegalArgumentException("Invalid direction: $dir")
+                WEST  -> "West"
+                else  -> throw IllegalArgumentException("Invalid direction: $dir")
             }
         }
     }
 
-    private var dxNS = 0
-    private var dxEW = 0
     private var curDir: Int = NORTH
+    private var lastLoc = MapLocation(0, 0)
+    private var curLoc = MapLocation(0, 0)
+    private var visited = HashSet<MapLocation>()
 
-    fun processMove(cmd: String) {
+    fun processMove(cmd: String): ArrayList<MapLocation> {
         val move = extractRotBy(cmd)
         curDir = nextDirection(move.rot, curDir)
-        updateVecDistance(curDir, move.by)
+
+        val nextLoc = nextLocation(curDir, move.by, curLoc)
+        val crossed = updateSeen(curLoc, nextLoc)
+
+        if (DEBUG) crossed.forEach { println("  crossed: $it") }
+
         if (DEBUG) println("${move.rot} -> ${move.by} " +
                 "(current direction: $curDir, " +
-                "dxNS: $dxNS, " +
-                "dxEW: $dxEW)")
+                "location: (${nextLoc.ew}, ${nextLoc.ns}))")
+
+        lastLoc = curLoc
+        curLoc = nextLoc
+
+        return crossed
     }
 
-    fun getDistanceFromStart(): Int {
-        return Math.abs(dxNS) + Math.abs(dxEW)
+    fun getDistanceFromStart(loc: MapLocation = curLoc): Int {
+        return Math.abs(loc.ns) + Math.abs(loc.ew)
     }
 
     fun getFinalDirection(): Int {
@@ -81,12 +100,49 @@ class TaxiCab(private val DEBUG: Boolean = false) {
         }
     }
 
-    private fun updateVecDistance(dir: Int, dist: Int) {
-        when (dir) {
-            NORTH -> dxNS += dist
-            SOUTH -> dxNS -= dist
-            EAST  -> dxEW += dist
-            WEST  -> dxEW -= dist
+    private fun nextLocation(dir: Int, dist: Int, loc: MapLocation): MapLocation {
+        return when (dir) {
+            NORTH -> MapLocation(loc.ew, loc.ns + dist)
+            SOUTH -> MapLocation(loc.ew, loc.ns - dist)
+            EAST  -> MapLocation(loc.ew + dist, loc.ns)
+            WEST  -> MapLocation(loc.ew - dist, loc.ns)
+            else  -> throw IllegalArgumentException("Invalid rotation")
         }
+    }
+
+    private fun updateSeen(cur: MapLocation, next: MapLocation): ArrayList<MapLocation> {
+        val crossed = ArrayList<MapLocation>()
+        var loc: MapLocation
+
+        if (DEBUG) println("move from: $cur -> $next")
+
+        var range = when (cur.ew < next.ew) {
+            true -> cur.ew until next.ew
+            false -> cur.ew downTo next.ew + 1
+        }
+
+        for (x in range) {
+            loc = MapLocation(x, cur.ns)
+            if (visited.contains(loc)) {
+                crossed.add(loc)
+            } else {
+                visited.add(loc)
+            }
+        }
+
+        range = when (cur.ns < next.ns) {
+            true -> cur.ns until next.ns
+            false -> cur.ns downTo next.ns + 1
+        }
+
+        for (y in range) {
+            loc = MapLocation(cur.ew, y)
+            if (visited.contains(loc)) {
+                crossed.add(loc)
+            } else {
+                visited.add(loc)
+            }
+        }
+        return crossed
     }
 }
